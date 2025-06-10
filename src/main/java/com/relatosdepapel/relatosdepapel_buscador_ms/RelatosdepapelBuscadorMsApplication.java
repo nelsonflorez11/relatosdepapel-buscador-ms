a4a368630626c0c82f55aac94de4b500f0e18310
Nelson Florez <nelsonflorez11@gmail.com>
2025-06-09 19:29:55 -0500

    Primer commit

---
+++ src/main/java/com/relatosdepapel/relatosdepapel_buscador_ms/RelatosdepapelBuscadorMsApplication.java
@@ -0,0 +1,13 @@
+package com.relatosdepapel.relatosdepapel_buscador_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatosdepapelBuscadorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatosdepapelBuscadorMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relatosdepapel/relatosdepapel_buscador_ms/RelatosdepapelBuscadorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relatosdepapel.relatosdepapel_buscador_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelatosdepapelBuscadorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
