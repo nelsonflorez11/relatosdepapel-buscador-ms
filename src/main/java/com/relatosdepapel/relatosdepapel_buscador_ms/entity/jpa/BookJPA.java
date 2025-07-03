0c05fa4d264f3b1cea4b4a6c3a051e2321147cdd
ROGER MAZA <born_to_kill1998@outlook.com>
2025-07-03 13:34:50 -0500

    ARchivo Book Model JPA

---
+++ src/main/java/com/relatosdepapel/relatosdepapel_buscador_ms/entity/jpa/BookJPA.java
@@ -0,0 +1,161 @@
+package com.relatosdepapel.relatosdepapel_buscador_ms.entity.jpa;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class BookJPA {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ISBN;
+
+    private String image;
+    private String author;
+    private String title;
+    private String description;
+    private String type;
+    private Double price;
+    private String editorial;
+    private String idioma;
+    private Integer paginas;
+    private Integer publicacion;
+    private String formato;
+    private String categoria;
+    private Integer valoracion;
+    private Integer stock;
+    private Boolean active;
+
+    public BookJPA() {
+    }
+
+    // Getters y Setters
+    public Long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(Long ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Integer paginas) {
+        this.paginas = paginas;
+    }
+
+    public Integer getPublicacion() {
+        return publicacion;
+    }
+
+    public void setPublicacion(Integer publicacion) {
+        this.publicacion = publicacion;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(Integer valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
+// Archivo eliminado para evitar duplicidad. Usa la versión en entity/.
